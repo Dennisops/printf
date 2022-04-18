49fd237f15d2d7de456bc38041fd4d4a457295a6
Dennisops <itsdennis254@gmail.com>
2022-04-18 20:07:17 +0000

    Changed header file

--- main.h
+++ main.h
@@ -1,6 +1,18 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
 int _printf(const char *format, ...);
+
+/**
+ * struct flags_printf - struct conversion to function
+ * @c: flag string
+ * @f: pointer to func
+ */
+
+typedef struct flags_printf
+{
+	char *c;
+	int (*f)(va_list);
+} flags_p;
 
 #endif
