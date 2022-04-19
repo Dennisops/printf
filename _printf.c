3a5f8d1a3db1c3c4efbbf5fe8ca237d7f5e4d6fd
MKipruto12 <mercykipruto2017@gmail.com>
2022-04-19 06:23:40 -0700

    modified

--- _printf.c
+++ _printf.c
@@ -8,55 +8,23 @@
  * _printf - produces output accrding to format
  * @format: character string composed of zero or more directives.
  *
- * Return:  the number of characters printed (excluding the null byte used * to end output to strings)
+ * Return: number of characters printed
  */
-
-
-int (*get_op(const char c))(va_list)
-{
-	int i = 0;
-
-	flags_p fp[] = {
-		{"c", print_char},
-		{"s", print_str},
-		{"%", print_percent}
-	};
-	while (i < 3)
-	{
-		if (c == fp[i].c[0])
-		{
-			return (fp[i].f);
-		}
-		i++;
-	}
-	return (0);
-}
 
 
 int _printf(const char *format, ...)
 {
-	va_list ap;
-	int sum = 0;
-	int i = 0;
-	int (*func)(va_list);
+	int sum = 0, i = 0;
 
-	va_start(ap, format);
-
-	while (format[i])
+	while (format[i] != '\0')
 	{
-		if (format[i] == '%')
-		{
-			func = get_op(format[i + 1]);
-			func(ap);
-			//increment i
-			i += 2;
-			continue;
-		}
-		_putchar(format[i]);
+		printf("%c", format[i]);
+		printf("%s", format[i]);
+
+
+		sum++;
 		i++;
 	}
-	va_end(ap);
+
 	return (sum);
 }
-
-
